081ff3b43027323a2f92f15c42c69953bbaf7da3
gluke0 <milanesigianluca@gmail.com>
2024-01-25 09:24:09 +0100

    added more buttons for diff animations

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ document.querySelector('.bounce-btn').addEventListener('click',()=>{
     document.querySelector('.nftCount').classList.remove('bounce');
     });
   });
+
+document.querySelector('.one-bounce').addEventListener('click',()=>{
+    document.querySelector('.nftCount').classList.add('bounce');
+  
+    document.querySelector('.nftCount').addEventListener('animationend',()=>{
+    document.querySelector('.nftCount').classList.remove('bounce');
+    });
+  });
+  
+document.querySelector('.super-bounce').addEventListener('click',()=>{
+    document.querySelector('.nftCount').classList.add('bounce');
+  
+    document.querySelector('.nftCount').addEventListener('animationend',()=>{
+    document.querySelector('.nftCount').classList.remove('bounce');
+    });
+  });
